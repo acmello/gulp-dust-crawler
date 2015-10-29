f6564e62714aeb4451011f3aa7391c6bd27cfae0
Karl Prieb <karl.prieb@iwsbrazil.com>
2015-10-29 18:21:15 -0200

    .

--- index.js
+++ index.js
@@ -15,64 +15,38 @@ var through = require('through2');
 var PLUGIN_NAME = 'gulp-dust-crawler';
 
 var gulpDustCrawler = function(options) {
-  return through.obj(function(file, enc, cb) {
+  var pagesPath = options.pages;
+  var templatesPath = options.templates;
+  var componentsPath = options.components;
 
-    console.log(file.contents.toString(enc));
+  var folders = [templatesPath, componentsPath, pagesPath];
 
-        // var content = modifier(String(file.contents));
-        // file.contents = new Buffer(content);
-        // this.push(file);
-        // done();
+  var jsonFiles = [];
+  var jsonData = [];
+  var pages = [];
+  var keys = {};
 
-      cb(null, file);
-    });
-  // var buffer = function(buf) {
-  //   // _.forEach(buf, function (b) {
-  //   //   console.log(b);
-  //   // })
-  //   console.log(buf);
-  // }
-  //
-  // var end = function() {
-  //   console.log('----END----');
-  // }
-  //
-  //
-  // return through.obj(buffer, end);
+  _.each(folders, function(folder) {
+    var file = glob.sync(folder + '/**/*.json');
+    jsonFiles = jsonFiles.concat(file);
+  });
+
+  _.each(jsonFiles, function (file) {
+    var parsedJSON = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+    if(parsedJSON.code) {
+      jsonFiles = _.without(jsonFiles, file);
+      pages = pages.concat(file);
+      keys[parsedJSON.code] = JSON.parse(fs.readFileSync(file, 'utf8'));
+    }
+
+    _.merge(jsonData, parsedJSON);
+
+  });
+
+  _.merge(jsonData, keys);
 
 
-  // var pagesPath = options.pages;
-  // var templatesPath = options.templates;
-  // var componentsPath = options.components;
-  //
-  // var folders = [templatesPath, componentsPath, pagesPath];
-  //
-  // var jsonFiles = [];
-  // var jsonData = [];
-  // var pages = [];
-  // var keys = {};
-  //
-  // _.each(folders, function(folder) {
-  //   var file = glob.sync(folder + '/**/*.json');
-  //   jsonFiles = jsonFiles.concat(file);
-  // });
-  //
-  // _.each(jsonFiles, function (file) {
-  //   var parsedJSON = JSON.parse(fs.readFileSync(file, 'utf8'));
-  //
-  //   if(parsedJSON.code) {
-  //     jsonFiles = _.without(jsonFiles, file);
-  //     pages = pages.concat(file);
-  //     keys[parsedJSON.code] = JSON.parse(fs.readFileSync(file, 'utf8'));
-  //   }
-  //
-  //   _.merge(jsonData, parsedJSON);
-  //
-  // });
-  //
-  // _.merge(jsonData, keys);
-  //
-  //
   // _.forEach(keys, function(key) {
   //   var compiled = dust.compile(
   //     fs.readFileSync('./src/pages/event.dust', 'utf8'), 'event');
@@ -87,5 +61,39 @@ var gulpDustCrawler = function(options) {
   //     }
   //   });
   // });
+
+  return through.obj(function(file, encode, callback) {
+    try {
+      var dustTemplate = file.contents.toString(encode || 'utf8');
+      var filePath = file.base;
+      var fileNameExt = file.path.split('/').pop()
+      var fileName = fileNameExt.split('.')[0];
+      var that = this;
+      var tempFile = null;
+      _.forEach(keys, function(key) {
+
+        var compiled = dust.compile(dustTemplate, fileName);
+        dust.loadSource(compiled);
+        dust.render(fileName, key, function (err, output) {
+
+          tempFile = new gutil.File({
+            base: file.base,
+            cwd: file.cwd,
+            path: file.path.replace('.dust', '.html').replace(fileName, key.code + '/' + fileName)
+          });
+
+          tempFile.contents = new Buffer(beautifyHtml(output));
+          that.push(tempFile);
+        });
+      });
+
+      return callback();
+
+    } catch (err) {
+      return callback(new gutil.PluginError('gulp-dust-crawler', err, options));
+    }
+
+    callback(null, file);
+  });
 };
 module.exports = gulpDustCrawler;
