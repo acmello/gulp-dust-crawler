84a397ab09fdf29db0714afd84459e92c47f0760
Karl Prieb <karl.prieb@iwsbrazil.com>
2015-10-29 15:01:42 -0200

    .

--- index.js
+++ index.js
@@ -8,55 +8,61 @@ var PluginError = gutil.PluginError;
 var fs = require('fs');
 var path = require('path');
 var glob = require('glob');
+var beautifyHtml = require('js-beautify').html;
+var through = require('through2');
 
 // Consts
 var PLUGIN_NAME = 'gulp-dust-crawler';
 
 var gulpDustCrawler = function(options) {
-  var pagesPath = options.pages;
-  var templatesPath = options.templates;
-  var componentsPath = options.components;
-
-  var folders = [templatesPath, componentsPath, pagesPath];
-
-  var jsonData = function(folders) {
-    var jsonFiles = [];
-    var jsonData = [];
-    var pages = [];
-    var keys = {};
-    var regex =  /^(event|schedule|home)$/;
-
-    _.each(folders, function(folder) {
-      var file = glob.sync(folder + '/**/*.json');
-      jsonFiles = jsonFiles.concat(file);
-    });
-
-    _.each(jsonFiles, function (file) {
-      var parsedJSON = JSON.parse(fs.readFileSync(file, 'utf8'));
-
-      if(parsedJSON.code) {
-        jsonFiles = _.without(jsonFiles, file);
-        pages = pages.concat(file);
-        keys[parsedJSON.code] = JSON.parse(fs.readFileSync(file, 'utf8'));;
-      }
-
-      _.merge(jsonData, parsedJSON);
-
-    });
-
-    _.merge(jsonData, keys);
-
-    console.log(jsonData);
-  };
-
-  jsonData(folders);
+  return through.obj(function () {
+    gutil.log('test');
+  })
+  // var pagesPath = options.pages;
+  // var templatesPath = options.templates;
+  // var componentsPath = options.components;
+  //
+  // var folders = [templatesPath, componentsPath, pagesPath];
+  //
+  // var jsonFiles = [];
+  // var jsonData = [];
+  // var pages = [];
+  // var keys = {};
+  //
+  // _.each(folders, function(folder) {
+  //   var file = glob.sync(folder + '/**/*.json');
+  //   jsonFiles = jsonFiles.concat(file);
+  // });
+  //
+  // _.each(jsonFiles, function (file) {
+  //   var parsedJSON = JSON.parse(fs.readFileSync(file, 'utf8'));
+  //
+  //   if(parsedJSON.code) {
+  //     jsonFiles = _.without(jsonFiles, file);
+  //     pages = pages.concat(file);
+  //     keys[parsedJSON.code] = JSON.parse(fs.readFileSync(file, 'utf8'));
+  //   }
+  //
+  //   _.merge(jsonData, parsedJSON);
+  //
+  // });
+  //
+  // _.merge(jsonData, keys);
+  //
+  //
+  // _.forEach(keys, function(key) {
+  //   var compiled = dust.compile(
+  //     fs.readFileSync('./src/pages/event.dust', 'utf8'), 'event');
+  //   dust.loadSource(compiled);
+  //   dust.render('event', key, function (error, output) {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //       // var generatedFile = './dist/pages/' + key.code + '-event.html';
+  //       // fs.writeFileSync(generatedFile, beautifyHtml(output));
+  //       return beautifyHtml(output);
+  //     }
+  //   });
+  // });
 };
-
-gulpDustCrawler(
-  {
-    pages: 'test/pages',
-    templates: 'test/templates',
-    components: 'test/components'
-  }
-);
 module.exports = gulpDustCrawler;
